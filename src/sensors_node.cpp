fa7c4be9d1e804050f56901a4cc600ecdb0df335
Omri <beta_b0t@yahoo.com>
2020-01-02 18:00:39 +0200

    publish odometry

--- src/sensors_node.cpp
+++ src/sensors_node.cpp
@@ -1,10 +1,15 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
 #include <libplayerc/playerc.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("sensors_node");
 
@@ -18,9 +23,12 @@ int main(int argc, char *argv[]) {
         RCLCPP_INFO(node->get_logger(), "Successfully connected to Player server!");
 
         auto robot_laser = playerc_laser_create(playerc_client, 0);
-        if (playerc_laser_subscribe(robot_laser, PLAYER_OPEN_MODE) == 0)
+        auto robot_position2d = playerc_position2d_create(playerc_client, 0);
+
+        if (playerc_laser_subscribe(robot_laser, PLAYER_OPEN_MODE) == 0 &&
+            playerc_position2d_subscribe(robot_position2d, PLAYER_OPEN_MODE) == 0)
         {
-            RCLCPP_INFO(node->get_logger(), "Successfully subscribed to laser!");
+            RCLCPP_INFO(node->get_logger(), "Successfully subscribed all Player topics!");
 
             auto laser_publisher = node->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::QoS(10));
 
@@ -34,7 +42,23 @@ int main(int argc, char *argv[]) {
             laser_msg.range_min = 0.0;
             laser_msg.range_max = 10.0;
 
+            tf2_ros::TransformBroadcaster transform_broadcaster(node);
+            tf2::Quaternion q;
+            geometry_msgs::msg::TransformStamped odom_tf_msg;
+
+            odom_tf_msg.header.frame_id = "odom";
+            odom_tf_msg.child_frame_id = "base_link";
+
+            auto odom_publisher = node->create_publisher<nav_msgs::msg::Odometry>("odom", rclcpp::QoS(10));
+            nav_msgs::msg::Odometry odom_msg;
+            odom_msg.header.frame_id = "odom";
+            odom_msg.child_frame_id = "base_link";
+            for (unsigned int i = 0; i < odom_msg.pose.covariance.size(); ++i) {
+                odom_msg.pose.covariance[i] = 0.0;
+            }
+
             rclcpp::Rate rate(10); // 10hz
+            rclcpp::Time now;
 
             while (rclcpp::ok())
             {
@@ -43,8 +67,39 @@ int main(int argc, char *argv[]) {
                 {
                     laser_msg.ranges[(robot_laser->scan_count - 1) - i] = (float)robot_laser->ranges[i];
                 }
-                laser_msg.header.stamp = node->now();
+                now = node->now();
+                laser_msg.header.stamp = now;
                 laser_publisher->publish(laser_msg);
+
+                q.setRPY(0.0, 0.0, robot_position2d->pa);
+
+                odom_msg.header.stamp = now;
+                odom_msg.pose.pose.position.x = robot_position2d->px;
+                odom_msg.pose.pose.position.y = robot_position2d->py;
+                odom_msg.pose.pose.position.z = 0.0;
+                odom_msg.pose.pose.orientation.x = q.x();
+                odom_msg.pose.pose.orientation.y = q.y();
+                odom_msg.pose.pose.orientation.z = q.z();
+                odom_msg.pose.pose.orientation.w = q.w();
+                odom_msg.twist.twist.linear.x = robot_position2d->vx;
+                odom_msg.twist.twist.linear.y = robot_position2d->vy;
+                odom_msg.twist.twist.linear.z = 0.0;
+                odom_msg.twist.twist.angular.x = 0.0;
+                odom_msg.twist.twist.angular.y = 0.0;
+                odom_msg.twist.twist.angular.z = robot_position2d->va;
+
+                odom_publisher->publish(odom_msg);
+
+                odom_tf_msg.header.stamp = now;
+                odom_tf_msg.transform.translation.x = robot_position2d->px;
+                odom_tf_msg.transform.translation.y = robot_position2d->py;
+                odom_tf_msg.transform.translation.z = 0.0;
+                odom_tf_msg.transform.rotation.x = q.x();
+                odom_tf_msg.transform.rotation.y = q.y();
+                odom_tf_msg.transform.rotation.z = q.z();
+                odom_tf_msg.transform.rotation.w = q.w();
+                transform_broadcaster.sendTransform(odom_tf_msg);
+
                 rate.sleep();
             }
 
